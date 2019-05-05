fcf4084ce07b2f17dc8cb0c94857a7c4d5675d9a
Sergei Legkodymov <rutven@gmail.com>
2019-05-05 22:24:51 +0300

    Initial import

---
+++ src/test/java/info/duty/planner/dutyspring/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package info.duty.planner.dutyspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
